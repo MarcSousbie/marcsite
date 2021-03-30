075547c2d60edb71ef49c019ea6c2d6165f3c5a9
Marc Sousbie <marc.sousbie@protonmail.com>
2021-03-30 16:46:27 +0200

    changed favicon and added social media links

--- components/SocialMedia.vue
+++ components/SocialMedia.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <v-btn
-      v-for="(item, i) in items.slice(0, large ? items.length : 3)"
+      v-for="(item, i) in items.slice(0, large ? items.length : 4)"
       :key="i"
       fab
       class="ma-2"
       color="info"
       :small="!large"
+      :href="item.link"
     >
-      <v-icon dark v-text="item.icon" />
+      <NuxtLink v-if="item.name == 'Email'" to="#get-in-touch">
+        <v-icon dark v-text="item.icon" />
+      </NuxtLink>
+      <v-icon v-else dark v-text="item.icon" />
     </v-btn>
   </div>
 </template>
@@ -27,22 +31,21 @@ export default {
       {
         name: 'Twitter',
         icon: 'mdi-twitter',
+        link: 'https://twitter.com/MarcSousbie',
       },
       {
-        name: 'Instagram',
-        icon: 'mdi-instagram',
+        name: 'Linkedin',
+        icon: 'mdi-linkedin',
+        link: 'https://www.linkedin.com/in/marc-sousbie-0b5a1943/',
       },
       {
         name: 'Facebook',
         icon: 'mdi-facebook',
+        link: 'https://www.facebook.com/marc.sousbie',
       },
       {
         name: 'Email',
         icon: 'mdi-email',
-      },
-      {
-        name: 'Phone',
-        icon: 'mdi-phone',
       },
     ],
   }),
