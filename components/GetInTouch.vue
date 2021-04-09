c52050d7a6c0f6e4cc0faab463ae1d3bd8c07150
Marc Sousbie <marc.sousbie@protonmail.com>
2021-04-09 11:33:46 +0200

    Hopefully made the contact form work.

--- components/GetInTouch.vue
+++ components/GetInTouch.vue
@@ -7,30 +7,51 @@
         <base-heading class="mb-5"> {{ $t('getintouch.title') }} </base-heading>
 
         <v-sheet color="transparent" max-width="600">
-          <v-text-field color="info" :label="$t('getintouch.name')" solo flat />
+          <v-form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact" />
 
-          <v-text-field
-            color="info"
-            :label="$t('getintouch.email')"
-            solo
-            flat
-          />
+            <v-text-field
+              color="info"
+              :label="$t('getintouch.name')"
+              :placeholder="$t('getintouch.name')"
+              solo
+              flat
+            />
 
-          <v-text-field
-            color="info"
-            :label="$t('getintouch.subject')"
-            solo
-            flat
-          />
+            <v-text-field
+              v-model="email"
+              :rules="[rules.required, rules.email]"
+              color="info"
+              :label="$t('getintouch.email')"
+              :placeholder="$t('getintouch.email')"
+              solo
+              flat
+            />
 
-          <v-textarea
-            color="info"
-            :label="$t('getintouch.message')"
-            solo
-            flat
-          />
+            <v-text-field
+              color="info"
+              :label="$t('getintouch.subject')"
+              :placeholder="$t('getintouch.subject')"
+              solo
+              flat
+            />
 
-          <base-btn>{{ $t('getintouch.send') }}</base-btn>
+            <v-textarea
+              :rules="[rules.required]"
+              color="info"
+              :label="$t('getintouch.message')"
+              :placeholder="$t('getintouch.message')"
+              solo
+              flat
+            />
+
+            <base-btn type="submit">{{ $t('getintouch.send') }}</base-btn>
+          </v-form>
         </v-sheet>
       </v-col>
 
@@ -40,3 +61,20 @@
     </v-row>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      rules: {
+        required: (value) => !!value || 'Required.',
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        },
+      },
+    }
+  },
+}
+</script>
